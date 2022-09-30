dfb1f97b26bb3a6519d9d74720cc551e9bb3c9b8
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-09-30 08:22:35 +0700

    Create TheTree.java

---
+++ src/Modul3/TheTree.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TheTree {
+    public static void main(String[] args){
+        System.out.println("I think I shall never see,\n"+"a poem as lovely as a tree.\n"+"A tree whose hungry mouth is pressed\n"+"Against the Earth's sweet flowing breast.");
+    }
+}
