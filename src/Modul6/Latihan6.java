e0b12e7a9e24a87ab6eef1a34fe07c1b9589e14e
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-10-27 21:18:33 +0700

    Create Latihan6.java

---
+++ src/Modul6/Latihan6.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul6;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan6 {
+    public static void main (String[] args){
+        
+        //do-while
+        
+         int r=0;
+        do{
+            System.out.println("Ulfah");
+            r++;
+        }while(true);
+    }
+}
