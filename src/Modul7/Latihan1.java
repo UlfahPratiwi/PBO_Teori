8ab2235cc3b1eed45fc484e3de3432b51e265e0c
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-11-03 21:43:45 +0700

    Create Latihan1.java

---
+++ src/Modul7/Latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul7;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan1 {
+     public static void main(String[] args){
+        
+        //for loop - array
+        String days[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+        
+          for(int i=0; i<days.length; i++){
+            System.out.println(days[i]);
+        }
+          
+    }
+}
