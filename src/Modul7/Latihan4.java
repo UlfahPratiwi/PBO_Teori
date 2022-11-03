2162920d0f6a3e9f58bba336e20cab92871581f4
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-11-03 21:59:51 +0700

    Create Latihan4.java

---
+++ src/Modul7/Latihan4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul7;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan4 {
+    public static void main(String[] args) {
+        
+        // do-while ---> lanjutan latihan1
+        
+String [] day = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        int i = 0;
+        
+        do{
+            System.out.println(day[i]);
+            i++;
+        }while(i<day.length);
+    }
+}
